141107739f12a358046cd44be85d11609cd7fa1e
vlad998 <63716492+vlad998@users.noreply.github.com>
2021-07-03 17:32:08 +0300

    Add files via upload

---
+++ fill-project/fill/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace fill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 267);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Genereaza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 174);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 220);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 153);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "n,linii";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 199);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "m,coloane";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(19, 100);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 28);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "joc nou";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "sterge tot";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Tag = "are tag";
+            this.button3.Text = "recoloreaza buton";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 405);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ fill-project/fill/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace fill
+{
+    public partial class Form1 : Form
+    {
+        int l = 0, c = 0;//coloana
+        int coloreaza = 0,count=0;
+        Button[,] butoane=new Button[50,50];//matrice de butoane
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//genereaza
+        {
+            ////input gresit,functioneaza doar data se introduc doar cifre sau caractere separat
+            //int result = 0;           
+            //bool box1 = int.TryParse(textBox1.Text, out result);
+            //bool box2 = int.TryParse(textBox1.Text, out result);
+            //if((box1==false)||(box2=false))
+            //{
+            //    MessageBox.Show("introdu doar cifre");\
+            //    if (box1 == false)
+            //        textBox1.Clear();
+            //    if (box2 == false)
+            //        textBox1.Clear();
+            //    return;
+            //}
+            ////
+            int n = int.Parse(textBox1.Text);//linie
+            int m = int.Parse(textBox2.Text);//coloana
+             
+            l = n;c = m;
+            int linie=140,coloana=0;
+            for (int i = 0; i < m; i++)
+            {
+                coloana = 10;//pixeli!!!
+                for (int j = 0; j < n; j++)
+                {  //pozitia e variabila
+
+                    butoane[i, j] = new Button();
+
+                    butoane[i, j].Click += new EventHandler(button_tag);
+                    butoane[i, j].Tag = i.ToString() + " " + j.ToString();
+                    coloana = coloana + 10;
+                    butoane[i,j].Location = new Point(linie,coloana);
+                    butoane[i, j].Size = new Size(50, 50);
+                    this.Controls.Add(butoane[i,j]);
+                    coloana = coloana + 50;
+                }
+                linie = linie + 55;//in pixeli          
+
+            }
+            if ((l == 0) || (c == 0))
+            {
+                MessageBox.Show("apasa pe genereaza");
+                return;
+            }
+            Random nr = new Random();
+            int numar = 0;
+            for (int i = 0; i < c; i++)
+            {
+                for (int j = 0; j < l; j++)
+                {
+                    numar = nr.Next(0, 2);
+                    if (numar == 0)
+                    {
+                        butoane[i, j].BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        butoane[i, j].BackColor = Color.Blue;
+                    }
+                }
+            }
+            button1.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+                          
+       
+        private void button_tag(object trimitator,EventArgs e )//trimitator
+        {
+            Button bt = (Button)trimitator;
+            //MessageBox.Show(bt.Tag.ToString());
+            string v=bt.Tag.ToString();
+            string[] k=v.Split();
+            int i = int.Parse(k[0]),j=int.Parse(k[1]);
+
+            //recoloreaza
+            if (coloreaza == 1)
+            {
+                if (butoane[int.Parse(k[0]), int.Parse(k[1])].BackColor == Color.Red)
+                    butoane[int.Parse(k[0]), int.Parse(k[1])].BackColor = Color.Blue;
+                else
+                    butoane[int.Parse(k[0]), int.Parse(k[1])].BackColor = Color.Red;
+                coloreaza = 0;
+                return;
+            }
+            //
+
+            for (int q=i-1;q<i+2;q++)
+             {
+                 for(int r=j-1;r<j+2;r++)
+                 {
+                     if((q>-1)&&(r>-1)&&(int.Parse(textBox1.Text)>r)&&(int.Parse(textBox2.Text)>q))
+                     if (butoane[q, r].BackColor == Color.Red)
+                     {
+                         butoane[q, r].BackColor = Color.Blue;
+                     }
+                     else
+                     {
+                         butoane[q, r].BackColor = Color.Red;
+                     }
+                 }
+             }
+            int nr=0,red=0,blue=0;
+             for (int w = 0; w < int.Parse(textBox2.Text); w++)
+             {
+                 for (int t = 0; t < int.Parse(textBox1.Text); t++)
+                 {
+                     nr++;
+                     if(butoane[w,t].BackColor==Color.Red)
+                         red++;
+                     if(butoane[w,t].BackColor==Color.Blue)
+                         blue++;
+                 }
+             }
+            count++;
+            if((red==nr)||(blue==nr))
+            {
+                MessageBox.Show("o culoare din"+count.ToString()+" nutari");
+                //System.Threading.Thread.Sleep(1000);
+            }
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)//sterge tot 
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)//recoloreaza buton
+        {
+            coloreaza = 1;
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar)&&(!char.IsControl(e.KeyChar)))
+                e.Handled = true;//daca-i true nu mai poti sa scrii
+            
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                if (!char.IsNumber(e.KeyChar)&&(!char.IsControl(e.KeyChar))&&!(e.KeyChar==97))
+                    e.Handled = true;
+          
+        }w
+       
+        private void button4_Click(object sender, EventArgs e)//joc nou
+        {
+            Application.Restart();
+            button1.Show();
+        }
+
+    }
+}
